c78d776be94ed8ea56eeaab8b448861d2e93adf3
Radeshjashti <jashtiradesh@gmail.com>
2026-07-24 21:57:04 +0530

    Add files via upload

---
+++ javs/index.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class index {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.print("Enter index: ");
+        int index=sc.nextInt();
+
+        if(index>=0 && index<n)
+        {
+            System.out.println("Element = "+arr[index]);
+        }
+        else
+        {
+            System.out.println("Invalid Index");
+        }
+
+    }
+
+}
